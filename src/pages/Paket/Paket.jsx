2a6327ced2bed46509e1deb069ab921fb08be8f5
Muhmmad Gimnastiar <adagimnas@gmail.com>
2023-11-18 10:48:06 +0700

    Add pages

---
+++ src/pages/Paket/Paket.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import '../../assets/css/tailwind.css'
+
+function Paket() {
+    return (  
+        <div className='bg-primary-orange'>Halaman Paket</div>
+    );
+}
+
+export default Paket;
