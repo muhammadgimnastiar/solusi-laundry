a692d55276b36f5869d2644264c2866a69c59738
Muhmmad Gimnastiar <adagimnas@gmail.com>
2023-11-18 22:14:42 +0700

    Add floating wa button, update hero, and theme

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,18 +6,18 @@ import {
   IconButton,
 } from "@material-tailwind/react";
 
-import {Link} from 'react-router-dom'
- 
+import { Link } from "react-router-dom";
+
 export default function StickyNavbar() {
   const [openNav, setOpenNav] = React.useState(false);
- 
+
   React.useEffect(() => {
     window.addEventListener(
       "resize",
-      () => window.innerWidth >= 960 && setOpenNav(false),
+      () => window.innerWidth >= 960 && setOpenNav(false)
     );
   }, []);
- 
+
   const navList = (
     <ul className="mt-2 mb-4 lg:px-20 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-14">
       <Typography
@@ -26,8 +26,9 @@ export default function StickyNavbar() {
         color="blue-gray"
         className="p-1 font-normal font-inter text-lg"
       >
-                <Link to={"/"} className="Flex items-center">Beranda</Link>
-
+        <Link to={"/"} className="Flex items-center">
+          Beranda
+        </Link>
       </Typography>
       <Typography
         as="li"
@@ -35,7 +36,9 @@ export default function StickyNavbar() {
         color="blue-gray"
         className="p-1 font-normal font-inter text-lg"
       >
-        <Link to={"/Pesanan"} className="Flex items-center">Pesanan</Link>
+        <Link to={"/Pesanan"} className="Flex items-center">
+          Pesanan
+        </Link>
       </Typography>
       <Typography
         as="li"
@@ -43,65 +46,65 @@ export default function StickyNavbar() {
         color="blue-gray"
         className="p-1 font-normal font-inter text-lg"
       >
-                <Link to={"/Paket"} className="Flex items-center">Paket</Link>
-
+        <Link to={"/Paket"} className="Flex items-center">
+          Paket
+        </Link>
       </Typography>
-      
     </ul>
   );
- 
+
   return (
-    
-      <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
-        <div className="flex items-center justify-between text-blue-gray-900">
-          <img src="./logo.png" className="mr-4 cursor-pointer " width={90} alt="" />
-          <div className="flex items-center gap-4">
-            <div className="mr-4 hidden lg:block">{navList}</div>
-            
-            <IconButton
-              variant="text"
-              className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
-              ripple={false}
-              onClick={() => setOpenNav(!openNav)}
-            >
-              {openNav ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  className="h-6 w-6"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                </svg>
-              )}
-            </IconButton>
-          </div>
+    <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
+      <div className="flex items-center justify-between text-blue-gray-900">
+        <img
+          src="./logo.png"
+          className="mr-4 cursor-pointer "
+          width={90}
+          alt=""
+        />
+        <div className="flex items-center gap-4">
+          <div className="mr-4 hidden lg:block">{navList}</div>
+
+          <IconButton
+            variant="text"
+            className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+            ripple={false}
+            onClick={() => setOpenNav(!openNav)}
+          >
+            {openNav ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                className="h-6 w-6"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            )}
+          </IconButton>
         </div>
-        <MobileNav open={openNav}>
-          {navList}
-        </MobileNav>
-      </Navbar>
-      
-    
+      </div>
+      <MobileNav open={openNav}>{navList}</MobileNav>
+    </Navbar>
   );
 }
