6b92a7c5b677223eba775716b351715be9001c61
Muhmmad Gimnastiar <adagimnas@gmail.com>
2023-11-22 18:55:12 +0700

    add: section Layanan, testimoni in home

---
+++ src/pages/Home/data.jsx
@@ -0,0 +1,41 @@
+export const Kelebihan = [
+  "Keharuman cucian akan bertahan lama (lebih dari 2 minggu) karena kami menggunakan pewangi yang kami gunakan adalah parfum premium grade A dengan berbagai pilihan aroma sesuai selera konsumen.",
+
+  "Proses pencucian pakaian satuan, kami lakukan dengan mengikuti prosedur pencucian berdasarkan washing care label tips (petunjuk cara pencucian) yang tertera pada tiap pakaian.",
+
+  "Proses penyetrikaan dilakukan dengan setrika khusus sehingga sangat aman untuk semua jenis pakaian dan tidak akan meninggalkan bekas",
+
+  "Kami melakukan managemen barang yang baik sehingga dapat meminimalisasi resiko tertukar/hilang dan proses pencucian kami di lakukan 1 mesin cuci untuk 1 costumer.",
+];
+
+export const Layanan = [
+  "Laundry Kiloan",
+  "Laundry Satuan (Selimut, Bed Cover, dll)",
+  "Laundry Kilat 8 Jam",
+  "Paket 1 Hari",
+  "Paket Bulanan",
+  "Cuci Sepatu, Tas",
+];
+
+export const Testimoni = [
+  {
+    id: 1,
+    img: "./images/people-1.svg",
+    review: "Pelayanan sangat ramah harga juga terjangkau, mantap",
+  },
+  {
+    id: 2,
+    img: "./images/people-2.svg",
+    review: "Puas banget dah, wanginya tahan lama",
+  },
+  {
+    id: 3,
+    img: "./images/people-3.svg",
+    review: "Sesuai amanah, kelar selalu tepat waktu",
+  },
+  {
+    id: 4,
+    img: "./images/people-4.svg",
+    review: "Wangi, rapih, murah mantap",
+  },
+];
