f16653b9eb885e8d76421ccf3bb38869809676eb
Muhmmad Gimnastiar <adagimnas@gmail.com>
2023-12-07 16:30:09 +0700

    Modify: Paket page

--- src/pages/Paket/GridCard.jsx
+++ src/pages/Paket/GridCard.jsx
@@ -3,14 +3,17 @@ import { paketan } from './data'
 
 const GridCard = () => {
     return (
-        <div className='w-full grid grid-cols-4 gap-3  rounded-lg p-2'>
+        <div className='w-full grid grid-rows-2 grid-cols-4 gap-3  rounded-xl p-2'>
             {paketan.map((paket) =>{
                 return (
-                <div key={paket.id} className='bg-blue-500 h-40 text-center rounded-lg p-2 cursor-pointer   shadow duration-150 hover:scale-105 hover:shadow-md'>
-                    <p>{paket.title}</p>
-                    <p>{paket.price}</p>
-                    <p>{paket.desc}</p>
-                    <p className='pt-10'>{paket.const}</p>
+                <div key={paket.id} className='bg-blue-400 h-50 text-center rounded-xl  p-2 cursor-pointer shadow duration-150 hover:scale-105 hover:shadow-md'>
+                    <p className='text-white'>{paket.title}</p>
+                    <p className='text-white'>{paket.price}</p>
+                    <p className='text-white text-left'>&nbsp;&nbsp;&nbsp;{paket.desc}</p>
+                    <p className='text-white text-left'>&nbsp;&nbsp;&nbsp;{paket.desc2}</p>
+                    <p className='text-white text-left'>&nbsp;&nbsp;&nbsp;{paket.desc3}</p>
+                    <p className='text-white text-left'>&nbsp;&nbsp;&nbsp;{paket.desc4}</p>
+                    <p className='text-white pt-10'>{paket.const}</p>
                 </div>
                 )
                 })}
--- src/pages/Paket/Paket.jsx
+++ src/pages/Paket/Paket.jsx
@@ -11,13 +11,13 @@ function Paket() {
         </div>
         <div className='w-full bg-blue-400 rounded-lg p-2 my-5 justify-center cursor-pointer shadow duration-150 hover:scale-105 hover:shadow-md'>
             <div className='flex flex-col items-start'>
-            <p className='px-2 pt-2 text-2xl font-bold'>Paket Bulanan</p>
-            <p className='px-2 text-xl font-semibold'>Rp. 100.000</p>
+            <p className='px-2 pt-2 text-2xl font-bold text-white'>Paket Bulanan</p>
+            <p className='px-2 text-xl font-semibold text-white'>Rp. 100.000</p>
             <ul className='list-disc flex flex-col px-10 pt-10 pb-5 items-start'>
-            <li >Harga berlaku untuk cuci setrika dan antar jemput max 18 Kg (jika lebih dr 18 kg, maka ada tambahan 5.5k/kg) </li>
-            <li >Harga berlaku per orang</li>
-            <li >Paket tidak temasuk selimut, seprei, bed cover, sepatu, tas, topi</li>
-            <li >Paket dihitung per 30 hari</li>
+            <li className='text-white'>Harga berlaku untuk cuci setrika dan antar jemput max 18 Kg (jika lebih dr 18 kg, maka ada tambahan 5.5k/kg) </li>
+            <li className='text-white'>Harga berlaku per orang</li>
+            <li className='text-white'>Paket tidak temasuk selimut, seprei, bed cover, sepatu, tas, topi</li>
+            <li className='text-white'>Paket dihitung per 30 hari</li>
             </ul>
         </div>
         </div>
