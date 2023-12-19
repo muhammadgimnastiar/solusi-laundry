921742ed929332e53f27a52be7dc52210aaa086c
Muhmmad Gimnastiar <adagimnas@gmail.com>
2023-12-19 19:28:25 +0700

    Add: svg x

--- src/pages/Paket/GridCard.jsx
+++ src/pages/Paket/GridCard.jsx
@@ -31,20 +31,48 @@ const GridCard = () => {
             </p>
             <p className="text-white text-left mb-2">
               &nbsp;&nbsp;&nbsp;{paket.desc2}
-              <svg
-                className="w-4 h-4 float-right mr-2"
-                viewBox="0 0 19 15"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M2 8.42857L6.28571 12.7143L17 2"
-                  stroke="white"
-                  stroke-width="3.91588"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
+              {paket.id === 2 ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-x w-4 h-4 float-right mr-2"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M13.854 2.146a.5.5 0 0 1 0 .708l-11 11a.5.5 0 0 1-.708-.708l11-11a.5.5 0 0 1 .708 0Z"
+                    stroke="white"
+                    stroke-width="3"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />{" "}
+                  <path
+                    fill-rule="evenodd"
+                    d="M2.146 2.146a.5.5 0 0 0 0 .708l11 11a.5.5 0 0 0 .708-.708l-11-11a.5.5 0 0 0-.708 0Z"
+                    stroke="white"
+                    stroke-width="3"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  className="w-4 h-4 float-right mr-2"
+                  viewBox="0 0 19 15"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M2 8.42857L6.28571 12.7143L17 2"
+                    stroke="white"
+                    stroke-width="3.91588"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              )}
             </p>
             <p className="text-white text-left mb-2">
               &nbsp;&nbsp;&nbsp;{paket.desc3}
